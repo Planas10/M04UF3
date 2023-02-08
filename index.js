0602e69e9663d4e0a0c8da97ab62673ae7ba05d6
PC-clase-02-09 <marc.planas@enti.cat>
2023-02-08 13:35:47 +0100

    no message

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 const http = require("http");
 const fs = require("fs");
 
-http.createServer(function(request, response){
 
+function send_index (response)
+{
 	fs.readFile("index.html", function(err, data){
 		if (err){
 			console.error(err);
@@ -17,5 +18,36 @@ http.createServer(function(request, response){
 
 		response.end();
 	});
+}
 
+function send_player (response)
+{
+	fs.readFile("player.png", function(err, data){
+		if (err){
+			console.error(err);
+			return;
+		}
+
+		response.writeHead(200, {"Content-Type":"image/png"});
+		response.write(data);
+
+		response.end();
+	});
+}
+
+
+http.createServer(function(request, response){
+	console.log(request.url);
+
+	let url = request.url.split("/");
+
+	switch (url[1]){
+		case "player.png":
+			send_player(response);
+
+			break;
+
+		default:
+			send_index(response);
+	}
 }).listen(6969);
